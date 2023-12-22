0415bd82761d8621c548c704bb22db207767767d
Roman Koriachko <roman_koriachko@epam.com>
2023-12-22 14:12:27 +0200

    add structure for task 2

---
+++ src/components/CreateCourse/CreateCourse.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// type Props = {}
+
+const CreateCourse = () => {
+	return <div>CreateCourse</div>;
+};
+
+export default CreateCourse;
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,6 @@ export { CourseInfo } from './CourceInfo';
 export { Courses } from './Courses';
 export { EmptyCourseList } from './EmptyCourseList';
 export { Header } from './Header';
+export { CreateCourse } from './CreateCourse';
+export { Login } from './Login';
+export { Registration } from './Registration';
