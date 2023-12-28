8a32113591ec0a2993a969ac3c564de911780c35
Roman Koriachko <roman_koriachko@epam.com>
2023-12-28 15:37:46 +0200

    show users name in header when he is logged

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,14 +4,34 @@ import { Logo } from '../Header/components';
 import { Button } from '../../common';
 
 import './Header.scss';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Header = () => {
+	const localUserData = JSON.parse(localStorage.getItem('loginData'));
+	const navigation = useNavigate();
+	function onLogoutClick() {
+		localStorage.removeItem('loginData');
+		navigation('/login');
+	}
+
+	const location = useLocation();
+
 	return (
 		<header className='header'>
 			<div className='container'>
 				<div className='header-row'>
 					<Logo />
-					<Button buttonText={'login'} />
+					{location.pathname === '/login' ||
+					location.pathname === '/registration' ? undefined : localUserData ? (
+						<div className='header-name-and-btn-row'>
+							<p>{localUserData.user.name}</p>
+							<Button buttonText={'logout'} onClick={onLogoutClick} />
+						</div>
+					) : (
+						<div className='header-name-and-btn-row'>
+							<Button buttonText={'login'} />
+						</div>
+					)}
 				</div>
 			</div>
 		</header>
